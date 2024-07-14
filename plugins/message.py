6bbb907caa943fff4a804ec47f8af75cf2922e09
Zenova-Prime <rajsanskarchauhan@gmail.com>
2024-07-14 21:47:36 +0530

    Update message.py

--- plugins/message.py
+++ plugins/message.py
@@ -17,33 +17,34 @@ HELP_TXT = script.HELP_TXT
 ABOUT_TXT = script.ABOUT_TXT
 
 
+import yt_dlp
+
+
 @Client.on_message(filters.regex(r'https?:\/\/(?:www\.)?(?:m\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([a-zA-Z0-9_-]{11})$'))
 async def handle_youtube_link(bot, message):
     video_id = get_youtube_video_id(message.text)
-    youtube = YouTube(f'https://www.youtube.com/watch?v={video_id}')
-    thumbnail_url = youtube.thumbnail_url
-    title = youtube.title
-    description = youtube.description
-    formatted_text = f"<b>{title}</b>\n\n{description[:300]}{'...' if len(description) > 300 else ''} <a href='https://www.youtube.com/watch?v={video_id}''>Read more</a>\n\n\nOnly download videos that you have the right to download. Do not use this bot to download copyrighted content that you do not have permission to use.\nDo not use this bot to download content that is illegal or violates Telegram's terms of service.\nBe respectful to other users and do not use the bot to spam or harass others.\nThe bot can only download videos that are publicly available on YouTube.\nThe bot can only download videos up to a maximum file size of 2 GB.\nThe bot can only download videos that are available in a format that can be downloaded."
-    resolutions = []
+    ydl_opts = {'quiet': True}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(f'https://www.youtube.com/watch?v={video_id}', download=False)
+        thumbnail_url = info['thumbnail']
+        title = info['title']
+        description = info['description']
+        formatted_text = f"<b>{title}</b>\n\n{description[:300]}{'...' if len(description) > 300 else ''} <a href='https://www.youtube.com/watch?v={video_id}''>Read more</a>\n\n\nOnly download videos that you have the right to download. Do not use this bot to download copyrighted content that you do not have permission to use.\nDo not use this bot to download content that is illegal or violates Telegram's terms of service.\nBe respectful to other users and do not use the bot to spam or harass others.\nThe bot can only download videos that are publicly available on YouTube.\nThe bot can only download videos up to a maximum file size of 2 GB.\nThe bot can only download videos that are available in a format that can be downloaded."
+        resolutions = [f'{stream["height"]}p' for stream in info['formats'] if stream['vcodec'] != 'none']
 
-    try:
-        for stream in youtube.streams.filter(progressive=True):
-            resolutions.append(stream.resolution)
         buttons = [
             InlineKeyboardButton(text=resolution, callback_data=f"res_{video_id}:{resolution}") for resolution in resolutions
         ]
-        # add two new buttons for 'video' and 'audio' in a new row
-        t_buttons = []
 
+        t_buttons = []
         t_buttons.append(InlineKeyboardButton(
             text="Video", callback_data=f"type_{video_id}:video"))
         t_buttons.append(InlineKeyboardButton(
             text="Audio", callback_data=f"type_{video_id}:audio"))
 
-        # add a 'download' button in another new row
         d_buttons = [InlineKeyboardButton(
             text="Download", callback_data=f"download_{video_id}:n:n")]
+
         keyboard = [buttons, t_buttons, d_buttons]
         markup = InlineKeyboardMarkup(keyboard)
         await message.reply_photo(
@@ -51,8 +52,6 @@ async def handle_youtube_link(bot, message):
             caption=formatted_text,
             parse_mode=ParseMode.HTML,
             reply_markup=markup)
-    except Exception as e:
-        await bot.send_message(message.chat.id, f"Error: {e}")
 
 @Client.on_message(filters.regex(r"(?:(?:https?:)?//)?(?:www\.)?youtube\.com/playlist\?list=([a-zA-Z0-9_-]+)"))
 async def handle_youtube_playlist_link(bot, message):
@@ -65,8 +64,10 @@ async def handle_youtube_playlist_link(bot, message):
         else:
             return await bot.send_message(chat_id, 'Something went wrong; Youtube Link Not Found!')
 
-        playlist = Playlist(playlist_url)
-        playlist_title = playlist.title
+        ydl_opts = {'quiet': True}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(playlist_url, download=False)
+            playlist_title = info['title']
 
         formatted_text = f"<b>Playlist: {playlist_title}</b>\n\nOnly download videos that you have the right to download. Do not use this bot to download copyrighted content that you do not have permission to use.\nDo not use this bot to download content that is illegal or violates Telegram's terms of service.\nBe respectful to other users and do not use the bot to spam or harass others.\nThe bot can only download videos that are publicly available on YouTube.\nThe bot can only download videos up to a maximum file size of 2 GB.\nThe bot can only download videos that are available in a format that can be downloaded."
 
@@ -87,7 +88,7 @@ async def handle_youtube_playlist_link(bot, message):
         keyboard = [buttons, t_buttons, d_buttons]
         markup = InlineKeyboardMarkup(keyboard)
 
-        await bot.send_message(chat_id=chat_id, text=formatted_text ,reply_markup=markup)
+        await bot.send_message(chat_id=chat_id, text=formatted_text, reply_markup=markup)
 
     except Exception as e:
         await bot.send_message(message.chat.id, f"Error: {e}")
